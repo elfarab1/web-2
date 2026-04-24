491819fb9b459c222170fcff75f73f651c8673bb
masabi <rumah.masabi@gmail.com>
2026-04-24 11:16:49 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const databaseProyek = {
+    "sistem_meretas1": {
+        judul: "sistem pos untuk pt JMC IT Consultant",
+        keahlian: ["IT"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBiL5-alKaw0FMcSGX5Lq2q64rWt66Dj5ouWukAzR6Ng&s&ec=121643124",
+        latar: "ini adalah proses menjaga keamanan data",
+        tantangan: "menjaga setiap sistem keamanan dari seorang meretas data yang menggangu sistem kemanan tersebut.",
+        pelajaran: "menggunakan skil ini untuk hal yang positif",
+        link: ""
+    },
+    "sistem_meretas2": {
+        judul: "sistem pos untuk pt JMC IT Consultant",
+        keahlian: ["IT"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBiL5-alKaw0FMcSGX5Lq2q64rWt66Dj5ouWukAzR6Ng&s&ec=121643124",
+        latar: "ini adalah proses menjaga keamanan data",
+        tantangan: "menjaga setiap sistem keamanan dari seorang meretas data yang menggangu sistem kemanan tersebut.",
+        pelajaran: "menggunakan skil ini untuk hal yang positif",
+        link: ""
+    },
+
+    
+};
+
+
+                

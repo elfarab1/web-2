def2bfcbbedf5ff038d418e7c19e867a90687de2
masabi <rumah.masabi@gmail.com>
2026-04-25 08:48:32 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const databaseProyek = {
     "sistem_meretas1": {
         judul: "sistem pos untuk pt JMC IT Consultant",
         keahlian: ["IT"],
-        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBiL5-alKaw0FMcSGX5Lq2q64rWt66Dj5ouWukAzR6Ng&s&ec=121643124",
+        gambar: "https://www.freepik.com/free-photos-vectors/programmer-logo",
         latar: "ini adalah proses menjaga keamanan data",
         tantangan: "menjaga setiap sistem keamanan dari seorang meretas data yang menggangu sistem kemanan tersebut.",
         pelajaran: "menggunakan skil ini untuk hal yang positif",
